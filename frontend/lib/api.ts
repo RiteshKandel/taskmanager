453269ab8e3a290c8b649d4a40a54a20e102c264
RiteshKandel <kandelritesh@gmail.com>
2026-06-02 12:47:38 +0530

    add next.config production settings

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig, AxiosError } from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
 })
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
