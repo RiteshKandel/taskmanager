10fe76d62744c52e55f5b49ba13e314d71689709
RiteshKandel <kandelritesh@gmail.com>
2026-06-02 16:41:00 +0530

    chore: add debug endpoint

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
+import os
+from django.http import JsonResponse
+def debug_db(request): return JsonResponse({'db_len': len(os.environ.get('DATABASE_URL', '')), 'db_prefix': os.environ.get('DATABASE_URL', '')[:15]})
+
 urlpatterns = [
+    path('debug_db/', debug_db),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/',    views.LoginView.as_view(),    name='login'),
     path('logout/',   views.LogoutView.as_view(),   name='logout'),
